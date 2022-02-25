63e31c6ae523c6630aa3b5855063ea9eb5de01ec
Sarfo Kantanka Frimpong <sarfokantankafrimpong@gmail.com>
2022-02-25 15:16:34 -0500

    main app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom'; 
+import React from "react"; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MainSearch from "./components/MainSearch";
+import AllFish from "./components/AllFish";
+import FishCard from "./components/FishCard"
+import CompareFish from './components/CompareFish'
+import './App.css';
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      allFish:[],
+      compare:{fishAId:"",fishBId:""},
+      currentFish:{}
+    }
+  }
+   
+  componentDidMount(){
+    fetch('https://acnhapi.com/v1a/fish')
+    .then(resp=>resp.json())
+    .then(json=>this.setState({allFish:json}));
+  }
+  render=()=>{
+    
+    return (<main>
+        <Routes>
+            <Route path='/allfish' element={<AllFish allFish={this.state.allFish}/>}></Route>
+            <Route path='/search'  element={<MainSearch/>}></Route>
+            <Route path='/'  element={<MainSearch/>}></Route>
+            <Route path='/fish/:id' element={<FishCard ></FishCard>}></Route>
+            <Route path='/compare' element={<CompareFish object={this}/>}></Route>
+        </Routes>
+        
+    </main>)
+  }
 }
+
+
 
 export default App;
