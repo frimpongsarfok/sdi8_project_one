ec915daf3a42c7b4bad5c7551e64469404d8c722
Sarfo Kantanka Frimpong <sarfokantankafrimpong@gmail.com>
2022-02-25 15:17:19 -0500

    app index file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -6,7 +8,10 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <Router>
+         <App />
+     </Router>
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
